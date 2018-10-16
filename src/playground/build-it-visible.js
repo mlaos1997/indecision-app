a606c77a0ae42c9b327a9359a499077ae676f257
Marcelo Laos <mlaos1997@gmail.com>
2018-10-15 23:55:37 -0400

    update

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,26 +1,38 @@
+// VisibilityToggle - render, constructor, handleToggleVisibility
+// visibility -> false
+
+class VisibilityToggle extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        }
+    }
+
+        handleToggleVisibility() {
+            this.setState((prevState) => {
+                return {
+                    visibility: !prevState.visibility
+                }
+            })
+        }
+
+        render() {
+            return (
+                <div>
+                    <h1>Toggle Visibility</h1>
+                    <button onClick={this.handleToggleVisibility}>{this.state.visibility ? 'Hide Details': 'Show Details'}</button>
+                    {this.state.visibility && (
+                        <p>These are the details</p>
+                    )}
+                </div>
+            )
+        }
+    }
 
 
-const appRoot = document.getElementById('app');
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-let visibility = false;
 
-const toggleVisibility = () => {
-    visibility = !visibility;
-    render();
-}
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleVisibility}>{visibility ? 'Hide Details' : 'Show Details'}</button>
-            {visibility && (
-                <p>These are the details</p>
-            )}
-        </div>
-    )
-
-    ReactDOM.render(template, appRoot);
-};
-
-render();
